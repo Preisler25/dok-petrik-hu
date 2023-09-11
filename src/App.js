14a34117c3179898a75333929b49f8a03fe9d1b0
Preisler <preisler.andras@gmail.com>
2023-09-11 10:37:38 +0200

    main

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import { RouterProvider } from 'react-router';
+import { createBrowserRouter } from 'react-router-dom';
+
+import navbar from './navBar.svg'
 import './App.css';
+import MainPage from './elements/pages/mainPage';
+import HibaPage from './elements/pages/hibaPage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    errorElement: <div><img src={navbar}></img>404 Not found</div>,
+    children: [
+      {
+        path: "",
+        element: <MainPage />,
+      },
+      {
+        path: "needHelp",
+        element: <HibaPage />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider router={router} />
   );
 }
+
 
 export default App;
---
+++ src/elements/pages/hibaPage.js
@@ -0,0 +1,9 @@
+const HibaPage = () => {
+    return (
+        <div>
+            <h1>HibaPage</h1>
+        </div>
+    );
+};
+
+export default HibaPage;
