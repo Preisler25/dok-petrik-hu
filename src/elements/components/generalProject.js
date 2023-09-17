94e96c0dffa0999b454f5ba938951fe0d8b37de9
Preisler <preisler.andras@gmail.com>
2023-09-17 22:04:52 +0200

    main

--- src/elements/components/generalProject.js
+++ src/elements/components/generalProject.js
@@ -1,17 +1,19 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 
 const GeneralProject = (project_id) => {
-    axios.get('api/getGP/?p_id=' + project.project_id).then((response) => {
-        console.log(response);
-        setProject(response.data);
-    }).catch((error) => {
-        console.log(error);
-    });
 
     const [project, setProject] = useState({});
 
+    useEffect(() => {
+        axios.get('api/getGP/?p_id=' + project.project_id).then((response) => {
+            console.log(response);
+            setProject(response.data);
+        }).catch((error) => {
+            console.log(error);
+        });
+    }, []);
     return (
         <div>
             <h1>General Project</h1>
