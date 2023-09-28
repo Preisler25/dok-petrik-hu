1e2d45b4ad40f4baa8e9a1724096fdf73bbe2f08
Preisler <preisler.andras@gmail.com>
2023-09-28 23:32:45 +0200

    main

--- src/elements/pages/projectPage.js
+++ src/elements/pages/projectPage.js
@@ -3,7 +3,7 @@ import GeneralProjectList from "../components/generalProjectList";
 const ProjectPage = () => {
     return (
         <section className="projectPage">
-            <div>
+            <div className="pp_a_t">
                 <h1>Aktív Projektjeink</h1>
             </div>
             <div className="projectList">
